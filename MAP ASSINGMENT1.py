4f0449cde147fa5d93b0b81dab884e64ac2f76b9
Akberkhan16 <94068972+Akberkhan16@users.noreply.github.com>
2021-12-04 21:46:24 +0530

    Add files via upload

---
+++ MAP ASSINGMENT1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Sample List: [4, 5, 2, 9]
+# Square the elements of the list:
+# [16, 25, 4, 81]
+
+
+def fun(n):
+    return n**2
+list(map(fun,[4, 5, 2, 9]))
+ 
+
+
+# In[ ]:
+
+
+
+
